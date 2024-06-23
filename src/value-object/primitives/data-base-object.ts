dc6b4fde6f82a05e9b41d354f1bfb8f8fc1d4079
Javier Vera Duran <jverad@MACJVERAD.local>
2024-06-23 17:05:15 -0400

    feat: csrf

--- src/value-object/primitives/data-base-object.ts
+++ src/value-object/primitives/data-base-object.ts
@@ -1,20 +1,28 @@
-const express = require('express')
-var pug = require('pug');
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const pug = require('pug');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
-    var input = req.query.username;
-    var template = `
+    const input = req.body.username;
+    const template = `
 doctype
 html
 head
     title= 'Hello world'
 body
     form(action='/' method='post')
-        input#name.form-control(type='text)
+        input#username.form-control(type='text' name='username' value='${input}')
         button.btn.btn-primary(type='submit') Submit
-    p Hello `+ input
-    var fn = pug.compile(template);
-    var html = fn();
+    p Hello ${input}`;
+    const fn = pug.compile(template);
+    const html = fn();
     res.send(html);
-})
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
