2844e10892befe7427d8369070f36b204e27eec0
Javier Vera Duran <jverad@MACJVERAD.local>
2024-06-23 18:42:36 -0400

    feat: add vulnera

---
+++ src/value-object/errores-object.ts
@@ -0,0 +1,15 @@
+// Consistencia 
+const value = 10;
+const resultadoFinal = value * 2;
+
+// Adaptabilidad
+const message: string = 10;
+
+// Responsabilidad
+
+class UserService {
+    getUserData() {
+    }
+    sendEmail() {
+    }
+}
