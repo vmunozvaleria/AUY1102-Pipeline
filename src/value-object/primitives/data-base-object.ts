908885a476e491b01bc6ed57b9e647634a3d1a28
Javier Vera Duran <jverad@MACJVERAD.local>
2024-06-23 16:54:35 -0400

    feat: sql inyection

--- src/value-object/primitives/data-base-object.ts
+++ src/value-object/primitives/data-base-object.ts
@@ -1,7 +1,9 @@
-// Intento de inyección SQL intencionado
 const username = "admin'; DROP TABLE Users; --";
 const password = 'password123';
 
-const queryString = `SELECT * FROM Users WHERE username='${username}' AND password='${password}'`;
+// Crear la consulta SQL de forma dinámica
+const queryString = `SELECT * FROM Users WHERE username='??' AND password='??'`;
+const query = db.prepare(queryString);
 
-console.log(queryString);
+// Ejecutar la consulta con parámetros
+query.run(username, password);
