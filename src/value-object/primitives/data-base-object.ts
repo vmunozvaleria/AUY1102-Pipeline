4d402d3b5f9214a1441f8d8d20f463ef593a6ad0
Javier Vera Duran <jverad@MACJVERAD.local>
2024-06-23 16:30:39 -0400

    feat: sql inyection

--- src/value-object/primitives/data-base-object.ts
+++ src/value-object/primitives/data-base-object.ts
@@ -1,6 +1,1 @@
-const username = "admin'; DROP TABLE Users; --";
-const password = 'password123';
-
-const queryString = `SELECT * FROM Users WHERE username='${username}' AND password='${password}'`;
-
-console.log(queryString);
+let unsafeQuery = "SELECT * FROM users WHERE username='\(userControlledString)'"
