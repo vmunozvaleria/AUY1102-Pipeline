e59bfc792a8b3a08bdc28a6f5ea5657e30f930f5
Javier Vera Duran <jverad@MACJVERAD.local>
2024-06-23 16:57:27 -0400

    feat: sql inyection

--- src/value-object/primitives/data-base-object.ts
+++ src/value-object/primitives/data-base-object.ts
@@ -1,6 +1,5 @@
-// Intento de inyección SQL intencionado
-const username = "admin'; DROP TABLE Users; --";
-const password = 'password123';
+const username = "' OR '1'='1'; --";
+const password = "' OR '1'='1'; --";
 
 const queryString = `SELECT * FROM Users WHERE username='${username}' AND password='${password}'`;
 
