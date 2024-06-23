cbaa1785e91092192def07842419673859fe18bc
Javier Vera Duran <jverad@MACJVERAD.local>
2024-06-23 17:11:40 -0400

    feat: change test

--- src/value-object/primitives/index.ts
+++ src/value-object/primitives/index.ts
@@ -2,4 +2,3 @@ export * from './enum-value-object';
 export * from './number-value-object';
 export * from './string-value-object';
 export * from './date-value-object';
-export * from './data-base-object';
