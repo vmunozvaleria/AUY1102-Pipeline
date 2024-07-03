b6602b42d205dacb1882471534d738c4b7980e26
Javier Vera Duran <jverad@MACJVERAD.local>
2024-07-02 21:04:21 -0400

    feat: add secret

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 export * from './value-object';
+
+// Este es un ejemplo de código JavaScript que expone una clave API sensible
+const apiKey = "12345-abcde-67890-fghij"; // Esto es un secreto
+
+function fetchData() {
+    fetch(`https://api.example.com/data?api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+}
+
+fetchData();
